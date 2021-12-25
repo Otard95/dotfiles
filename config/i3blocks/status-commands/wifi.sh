6b858fcae8845066868321f41a9ff76f8522144a
StianM <stian.myklebostad@schibsted.com>
2021-12-25 19:42:28 +0100

    Update i3 bar, use i3blocks

---
+++ config/i3blocks/status-commands/wifi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+status=$(nmcli -c no device wifi | grep '*')
+signal=$(awk '{print $8}' <<< "$status")
+ssid=$(awk '{print $3}' <<< "$status")
+
+echo "$signal% $ssid"
+echo "$signal%"
+
+[ $signal -le 10 ] exit 33
+
+if [ $signal -le 30 ]; then
+  echo "#ff9321"
+elif [ $signal -lt 50 ]; then
+  echo "#ffeb11"
+elif [ $signal -lt 75 ]; then
+  echo "#02aef2"
+else
+  echo "#14e002"
+fi
