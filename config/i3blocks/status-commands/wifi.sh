5337277e71eb0733162716a2b63ba214c46713a6
StianM <stian.myklebostad@schibsted.com>
2021-12-28 14:53:25 +0100

    Update wifi bar block with 'not connected' state

--- config/i3blocks/status-commands/wifi.sh
+++ config/i3blocks/status-commands/wifi.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 status=$(nmcli -c no device wifi | grep '*')
+
+if [[ -z "$status" ]]; then
+  echo " Not Connected"
+  echo ""
+  echo "#aaaaaa"
+fi
+
 signal=$(awk '{print $8}' <<< "$status")
 ssid=$(awk '{print $3}' <<< "$status")
 
