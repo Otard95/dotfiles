549eef16e20989ea843ec1dee7ef69f139529acc
StianM <stian.myklebostad@schibsted.com>
2022-12-21 07:11:37 +0100

    fix(i3): calc

--- config/i3/kitty-calc.sh
+++ config/i3/kitty-calc.sh
@@ -2,7 +2,7 @@
 PID=$(ps ux | grep 'kitty --class calculator' | grep -v 'grep' | grep -v 'bin' | awk '{print $2}')
 
 if [ -z "$PID" ]; then
-  kitty --class calculator '/home/otard/scripts/calc'
+  kitty --class calculator -d '/home/otard/Documents/quickmath' '/home/otard/scripts/calc'
 else
   kill $PID
 fi
