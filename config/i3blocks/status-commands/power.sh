183f663eaf479647c9d87be212f54bfd146cddf7
StianM <stian.myklebostad@schibsted.com>
2022-12-21 07:17:25 +0100

    feat: New power/sleep/lock/... dialog

---
+++ config/i3blocks/status-commands/power.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+ACTION=$(system-power-dialog --size small | grep 'system-power-select::')
+
+# Actions
+#  - shutdown
+#  - reboot
+#  - lock
+#  - logout
+#  - hibernate
+#  - sleep
+
+if [[ $ACTION == "system-power-select::shutdown" ]]; then
+    /sbin/shutdown -P now
+elif [[ $ACTION == "system-power-select::reboot" ]]; then
+    /sbin/shutdown -r now
+elif [[ $ACTION == "system-power-select::lock" ]]; then
+    i3-msg -q exec betterlockscreen -- -l
+elif [[ $ACTION == "system-power-select::logout" ]]; then
+    i3-msg exit
+elif [[ $ACTION == "system-power-select::hibernate" ]]; then
+    systemctl hibernate
+elif [[ $ACTION == "system-power-select::sleep" ]]; then
+    systemctl suspend
+fi
