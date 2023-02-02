cf2c7f8cc40dc547ce05c7b9f5a6aa86b43f93b9
StianM <stian.myklebostad@schibsted.com>
2023-02-02 10:50:24 +0100

    fix(mutlicursor): Select branch master

--- config/nvim/plug-conf/visual-multi.lua
+++ config/nvim/plug-conf/visual-multi.lua
@@ -1,8 +1,6 @@
 
-Plug 'mg979/vim-visual-multi', {'branch': 'master'}
-
 function visual_multi_init(paqInit)
-  table.insert(paqInit, { 'mg979/vim-visual-multi', { 'branch': 'master'} })
+  table.insert(paqInit, { 'mg979/vim-visual-multi', { branch = 'master'} })
 end
 
 return visual_multi_init
