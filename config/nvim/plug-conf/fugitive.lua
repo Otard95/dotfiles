000cd20536d73860e3a09abc366756f54e6c0cda
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-06-19 07:34:24 +0200

    feat(nvim): Add fugitive keybinds

--- config/nvim/plug-conf/fugitive.lua
+++ config/nvim/plug-conf/fugitive.lua
@@ -1,6 +1,18 @@
 
 function fugitive_init(paqInit)
   table.insert(paqInit, 'tpope/vim-fugitive')
+
+  -- ###############
+  -- # Keybindings #
+  -- ###############
+
+  -- Merge conflict resolution
+  vim.api.nvim_set_keymap('n', '<leader>gm', ':Gvdiffsplit!<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>gj', ':diffget //3<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>gf', ':diffget //2<CR>', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>gn', ']c', { noremap = true, silent = true })
+  vim.api.nvim_set_keymap('n', '<leader>gp', '[c', { noremap = true, silent = true })
+  
 end
 
 return fugitive_init
