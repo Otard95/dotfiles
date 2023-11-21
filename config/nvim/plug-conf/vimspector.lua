a6791a97976ece8a6049c0f3237d02123bf7bb9b
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-11-21 08:45:21 +0100

    chore(vimspector): Remap everything

--- config/nvim/plug-conf/vimspector.lua
+++ config/nvim/plug-conf/vimspector.lua
@@ -2,17 +2,17 @@
 function vimspector_init(paqInit)
   table.insert(paqInit, 'puremourning/vimspector')
 
-  vim.keymap.set('n', '<C-g><C-s>', '<Plug>VimspectorContinue', {})
-  vim.keymap.set('n', '<C-g><C-s><C-s>', '<Plug>VimspectorStop', {})
-  vim.keymap.set('n', '<C-g><C-r>', ':VimspectorReset<CR>', {})
-  vim.keymap.set('n', '<C-g><C-p>', '<Plug>VimspectorPause', {})
-  vim.keymap.set('n', '<C-g><C-b><C-b>', '<Plug>VimspectorToggleBreakpoint', {})
-  vim.keymap.set('n', '<C-g><C-b>c', '<Plug>VimspectorToggleConditionalBreakpoint', {})
-  vim.keymap.set('n', '<C-g><C-l>', '<Plug>VimspectorRunToCursor', {})
-  vim.keymap.set('n', '<C-g><C-n>', '<Plug>VimspectorStepOver', {})
-  vim.keymap.set('n', '<C-g><C-i>', '<Plug>VimspectorStepInto', {})
-  vim.keymap.set('n', '<C-g><C-o>', '<Plug>VimspectorStepOut', {})
-  vim.keymap.set('n', '<C-g><C-k>', '<Plug>VimspectorBalloonEval', {})
+  vim.keymap.set('n', '<leader>dr', '<Plug>VimspectorContinue', {})
+  vim.keymap.set('n', '<leader>dss', '<Plug>VimspectorStop', {})
+  vim.keymap.set('n', '<leader>dsr', ':VimspectorReset<CR>', {})
+  vim.keymap.set('n', '<leader>dp', '<Plug>VimspectorPause', {})
+  vim.keymap.set('n', '<leader>dbb', '<Plug>VimspectorToggleBreakpoint', {})
+  vim.keymap.set('n', '<leader>dbc', '<Plug>VimspectorToggleConditionalBreakpoint', {})
+  vim.keymap.set('n', '<leader>dl', '<Plug>VimspectorRunToCursor', {})
+  vim.keymap.set('n', '<leader>dn', '<Plug>VimspectorStepOver', {})
+  vim.keymap.set('n', '<leader>di', '<Plug>VimspectorStepInto', {})
+  vim.keymap.set('n', '<leader>do', '<Plug>VimspectorStepOut', {})
+  vim.keymap.set('n', '<leader>dk', '<Plug>VimspectorBalloonEval', {})
 
   vim.g.vimspector_sign_priority = {
      vimspectorBP = 13,
