2864f99dc5eccc263effb25eca5fc7b39ed48494
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-11-21 08:51:06 +0100

    chore(markdown-preview): Make more ergonomic

--- config/nvim/plug-conf/markdown-preview.lua
+++ config/nvim/plug-conf/markdown-preview.lua
@@ -1,9 +1,19 @@
 
+vim.cmd [[
+  function! MarkdownPreviewOpenBrowser(url)
+    call system('/opt/firefox/firefox -P markdown-preview ' . a:url . ' &')
+  endfunction
+]]
+
 function nvim_markdown_preview_init(paqInit)
   table.insert(paqInit, {
     'iamcco/markdown-preview.nvim',
     run = 'cd markdown-preview.nvim && yarn install'
   })
+
+  vim.g.mkdp_auto_close = 0
+  vim.g.mkdp_browserfunc = 'MarkdownPreviewOpenBrowser'
+  vim.g.mkdp_page_title = '「${name}」 - Markdown Preview'
 end
 
 return nvim_markdown_preview_init
