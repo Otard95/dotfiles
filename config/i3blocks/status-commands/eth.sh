dca8cfe2c80fb808112dfca2be393fc1cd09611c
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2023-11-21 08:54:16 +0100

    chore(i3blocks): Add eth status

---
+++ config/i3blocks/status-commands/eth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+status=$(nmcli d | grep 'ethernet' | grep -v 'unmanaged' | awk '{print $3}')
+
+if [[ "$status" != "connected" ]]; then
+  echo "$_left<span background=\"$_color\"> Disconnected</span>$_right"
+  echo "$_left<span background=\"$_color\"></span>$_right"
+  echo "#aaaaaa"
+  exit 0
+fi
+
+echo "$_left<span background=\"$_color\">  Connected</span>$_right"
+echo "$_left<span background=\"$_color\"></span>$_right"
+echo "#14e002"
