eecbf31164f6bbdbc1ccf57ac61b5dff5c448d10
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-01-02 12:58:47 +0100

    feat(nvim): Add git signes

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -7,6 +7,26 @@ return {
         delay = 500,
       },
       current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', '<leader>ghn', gs.next_hunk)
+        map('n', '<leader>ghp', gs.prev_hunk)
+
+        -- Actions
+        map('n', '<leader>ghs', gs.stage_hunk)
+        map('n', '<leader>ghr', gs.reset_hunk)
+        map('v', '<leader>ghs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('v', '<leader>ghr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+        map('n', '<leader>ghi', gs.preview_hunk)
+      end,
     },
   }
 }
