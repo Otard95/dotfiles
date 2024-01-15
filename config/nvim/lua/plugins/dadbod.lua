39f494c250d2785e1f6a411b13b85d89284c7fd8
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-01-15 13:14:13 +0100

    feat(nvim): Add dadbod

---
+++ config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,36 @@
+local function db_completion()
+  require("cmp").setup.buffer { sources = { { name = "vim-dadbod-completion" } } }
+end
+
+function SetupDadbod()
+  vim.g.db_ui_save_location = vim.fn.stdpath "config" .. require("plenary.path").path.sep .. "db_ui"
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "sql",
+    },
+    command = [[setlocal omnifunc=vim_dadbod_completion#omni]],
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = {
+      "sql",
+      "mysql",
+      "plsql",
+    },
+    callback = function()
+      vim.schedule(db_completion)
+    end,
+  })
+end
+
+return {
+  {
+    'tpope/vim-dadbod',
+    dependencies = {
+      'kristijanhusak/vim-dadbod-ui',
+      'kristijanhusak/vim-dadbod-completion',
+    },
+    config = SetupDadbod,
+  }
+}
