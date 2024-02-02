1898c1d6177087827e8db23aeae2a65445e2d02d
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-02-02 10:45:17 +0100

    feat(nvim): Use telescope to select env for nvim.rest

--- config/nvim/lua/plugins/rest.lua
+++ config/nvim/lua/plugins/rest.lua
@@ -1,3 +1,29 @@
+---@param callback function
+local function find_env(callback)
+  local pickers = require 'telescope.pickers'
+  local finders = require 'telescope.finders'
+  local sorters = require 'telescope.sorters'
+  local actions = require 'telescope.actions'
+  local actions_state = require 'telescope.actions.state'
+
+  local function call_with_selected(prompt_bufnr)
+    local selection = actions_state.get_selected_entry(prompt_bufnr)
+    actions.close(prompt_bufnr)
+    callback(selection.value)
+  end
+
+  pickers.new({}, {
+    prompt_title = 'Select .env file',
+    finder = finders.new_oneshot_job({ 'find', '.', '-type', 'f', '-name', '.env*' }),
+    sorter = sorters.get_fuzzy_file(),
+    attach_mappings = function(_, map)
+      map('i', '<CR>', call_with_selected)
+      map('n', '<CR>', call_with_selected)
+      return true
+    end
+  }):find()
+end
+
 function SetupRest()
   local rest = require 'rest-nvim'
 
@@ -5,6 +31,11 @@ function SetupRest()
     result_split_horizontal = true,
     result_split_in_place = true,
     stay_in_current_window_after_split = true,
+    result = {
+      show_statistics = {
+        'time_total'
+      },
+    },
   }
 
   vim.api.nvim_create_autocmd('FileType', {
@@ -12,9 +43,15 @@ function SetupRest()
     callback = function(ev)
       local opts = { buffer = ev.buf }
 
-      vim.keymap.set({ 'n', 'i' }, '<leader>rr', function() rest.run()     end, opts)
-      vim.keymap.set({ 'n', 'i' }, '<leader>rp', function() rest.run(true) end, opts)
-      vim.keymap.set({ 'n', 'i' }, '<leader>rl', function() rest.last()    end, opts)
+      vim.keymap.set({ 'n' }, '<leader>rr', function() rest.run() end, opts)
+      vim.keymap.set({ 'n' }, '<leader>rp', function() rest.run(true) end, opts)
+      vim.keymap.set({ 'n' }, '<leader>rl', function() rest.last() end, opts)
+      vim.keymap.set({ 'n' }, '<leader>rs', function()
+        find_env(function (file)
+          rest.select_env(file)
+          print('Using env file: ' .. file)
+        end)
+      end, opts)
     end
   })
 end
@@ -22,7 +59,10 @@ end
 return {
   {
     'rest-nvim/rest.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
     ft = { 'http', 'rest' },
     config = SetupRest,
   }
