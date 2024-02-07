e8ba0a4ce533b5606a154a35315e0fdb739b8062
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-02-07 08:50:40 +0100

    fix(nvim): Lualine inacive colors

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,35 +1,51 @@
+function SetupLualine()
+  local lualine = require 'lualine'
+  local auto = require 'lualine.themes.auto'
+  local palette = require 'catppuccin.palettes.frappe'
+
+  local inactive_colors = { fg = palette.blue, bg = palette.surface0 }
+  auto.inactive.a = inactive_colors
+  auto.inactive.b = inactive_colors
+  auto.inactive.c = inactive_colors
+
+  lualine.setup {
+    options = {
+      theme = auto,
+      component_separators = '|',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = {
+        { 'mode', separator = { left = '' }, right_padding = 2 },
+      },
+      lualine_b = { 'filename', 'branch' },
+      lualine_c = { 'fileformat' },
+      lualine_x = {},
+      lualine_y = { 'filetype', 'progress' },
+      lualine_z = {
+        { 'location', separator = { right = '' }, left_padding = 2 },
+      },
+    },
+    inactive_sections = {
+      lualine_a = { 'filename' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = { 'location' },
+    },
+    tabline = {},
+    extensions = {},
+  }
+end
+
 return {
   {
     'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', lazy = true },
-    opts = {
-      options = {
-        theme = 'catppuccin',
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
-      },
-      sections = {
-        lualine_a = {
-          { 'mode', separator = { left = '' }, right_padding = 2 },
-        },
-        lualine_b = { 'filename', 'branch' },
-        lualine_c = { 'fileformat' },
-        lualine_x = {},
-        lualine_y = { 'filetype', 'progress' },
-        lualine_z = {
-          { 'location', separator = { right = '' }, left_padding = 2 },
-        },
-      },
-      inactive_sections = {
-        lualine_a = { 'filename' },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = { 'location' },
-      },
-      tabline = {},
-      extensions = {},
+    dependencies = {
+      { 'nvim-tree/nvim-web-devicons', lazy = true },
+      { 'catppuccin/nvim' },
     },
+    config = SetupLualine,
   }
 }
