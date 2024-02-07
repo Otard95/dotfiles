475d907140f7538dfb8450e1bd5f6c5470415044
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-02-07 08:49:32 +0100

    feat(nvim): Update plugin keybinds

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -1,10 +1,21 @@
 function fugitive_init()
   -- Merge conflict resolution
-  vim.api.nvim_set_keymap('n', '<leader>gm', ':Gdiffsplit!<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', '<leader>gj', ':diffget //3<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', '<leader>gf', ':diffget //2<CR>', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', '<leader>gn', ']c', { noremap = true, silent = true })
-  vim.api.nvim_set_keymap('n', '<leader>gp', '[c', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>gm', ':Gdiffsplit!<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>gj', ':diffget //3<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>gf', ':diffget //2<CR>', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>gn', ']c', { noremap = true, silent = true })
+  vim.keymap.set('n', '<leader>gp', '[c', { noremap = true, silent = true })
+
+  -- Keybinds in status window
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "fugitive",
+    callback = function(e)
+      local opts = { buffer = e.buf, silent = true }
+
+      vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { buffer = true, silent = true })
+      vim.keymap.set('n', '<leader>gP', ':Git push -u origin HEAD<CR>', { noremap = true, silent = true })
+    end,
+  })
 end
 
 return {
--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -3,36 +3,19 @@ function SetupHarpoon()
 
   harpoon:setup()
 
-  local conf = require("telescope.config").values
-  local function toggle_quick_menu()
-    local file_paths = {}
-    for _, item in ipairs(harpoon:list().items) do
-        table.insert(file_paths, item.value)
-    end
+  vim.keymap.set('n', '<leader>a', function() harpoon:list():append() end)
+  -- vim.keymap.set('n', '<leader>he', toggle_quick_menu)
+  vim.keymap.set('n', '<leader>e', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-  end
-
-  vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
-  -- vim.keymap.set("n", "<leader>he", toggle_quick_menu)
-  vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-  vim.keymap.set("n", "<leader>hh", function() harpoon:list():select(1) end)
-  vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(2) end)
-  vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(3) end)
-  vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(4) end)
-  vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(5) end)
+  vim.keymap.set('n', '<leader>h', function() harpoon:list():select(1) end)
+  vim.keymap.set('n', '<leader>j', function() harpoon:list():select(2) end)
+  vim.keymap.set('n', '<leader>k', function() harpoon:list():select(3) end)
+  vim.keymap.set('n', '<leader>l', function() harpoon:list():select(4) end)
+  vim.keymap.set('n', '<leader>;', function() harpoon:list():select(5) end)
 
   -- Toggle previous & next buffers stored within Harpoon list
-  vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
-  vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
+  vim.keymap.set('n', '<leader>p', function() harpoon:list():prev() end)
+  vim.keymap.set('n', '<leader>n', function() harpoon:list():next() end)
 end
 
 return {
@@ -40,9 +23,6 @@ return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     config = SetupHarpoon,
-    dependencies = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope.nvim' },
-    }
+    dependencies = { 'nvim-lua/plenary.nvim' }
   }
 }
