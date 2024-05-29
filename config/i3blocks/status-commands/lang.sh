070bc42ccca2c5f2066f207bf0749a9153012ba9
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-05-29 07:41:22 +0200

    chore(i3blocks): Add some space between icon and lang text

--- config/i3blocks/status-commands/lang.sh
+++ config/i3blocks/status-commands/lang.sh
@@ -2,5 +2,5 @@
 
 lang=$(setxkbmap -query | awk '/layout/{print $2}')
 
-echo "$_left<span background=\"$_color\"> $lang</span>$_right"
-echo "$_left<span background=\"$_color\"> $lang</span>$_right"
+echo "$_left<span background=\"$_color\">  $lang</span>$_right"
+echo "$_left<span background=\"$_color\">  $lang</span>$_right"
