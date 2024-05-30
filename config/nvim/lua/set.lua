e663ee7d91147552625e900d9709a8390a2bd710
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-05-30 07:50:54 +0200

    feat(nvim): Set no-backup/swap, add undodir and set undofile

--- config/nvim/lua/set.lua
+++ config/nvim/lua/set.lua
@@ -15,7 +15,6 @@ vim.opt.termguicolors = true
 -- Folding
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-
 vim.opt.foldlevel = 2
 
 -- Search
@@ -27,11 +26,18 @@ vim.opt.wrap = true
 vim.opt.showbreak = '↪ '
 vim.opt.linebreak = true
 vim.opt.colorcolumn = '80'
+vim.opt.signcolumn = 'yes'
+
+-- Undo, swap, backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.local/state/nvim/undodir"
+vim.opt.undofile = true
 
 -- Misc
 vim.opt.wildmode = 'longest:full,full'
 vim.opt.list = true
-vim.opt.listchars = 'tab:-> ,trail:⋅'
+vim.opt.listchars = 'tab:󰌒 ,trail:⋅'
 vim.opt.scrolloff = 20
 vim.opt.sidescrolloff = 10
 vim.opt.clipboard = 'unnamedplus'
@@ -39,4 +45,3 @@ vim.opt.cmdheight = 2
 vim.opt.mouse = 'a'
 vim.opt.updatetime = 50
 vim.opt.formatoptions:remove { 'o' }
-vim.opt.signcolumn = 'yes'
