019632d213c7e7b10ed7116da86d7b541d8095f9
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-06-04 20:52:07 +0200

    feat(polybar): Setup per hostname config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -4,7 +4,7 @@ polybar-msg cmd quit
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m HOSTCONFIG=$(hostname).ini polybar --reload example &
   done
 else
   polybar --reload example &
