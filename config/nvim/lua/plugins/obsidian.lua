e5ceb1655be9c5d86cac9509d5f029776097b024
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-07-24 15:00:55 +0200

    fix(nvim): Obsidian conf

--- config/nvim/lua/plugins/obsidian.lua
+++ config/nvim/lua/plugins/obsidian.lua
@@ -28,7 +28,10 @@ end
 
 ---@param url string
 local function follow_url(url)
-  vim.fn.jobstart({ 'open', url })
+  if path.is_relative(url) then
+    url = path.resolve(path.pathname(vim.api.nvim_buf_get_name(0)), url)
+  end
+  vim.fn.jobstart({"open", url})
 end
 
 return {
@@ -79,12 +82,6 @@ return {
           end,
         },
       },
-      follow_url_func = function(url)
-        if path.is_relative(url) then
-          url = path.resolve(path.pathname(vim.api.nvim_buf_get_name(0)), url)
-        end
-        vim.fn.jobstart({"open", url})
-      end,
       daily_notes = {
         folder = 'Dailies',
         date_format = '%Y/%b/%d',
