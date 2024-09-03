881788fbcc60c6a0ef30dd7520099b2c6cafedf2
Stian Myklebostad (Work) <stian.myklebostad@schibsted.com>
2024-09-03 10:13:27 +0200

    feat(nvim): Center view on lsp hover

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -134,7 +134,10 @@ function SetupLsp()
 
       vim.keymap.set('n', 'gd', fn.flow(center_after_buf_enter, vim.lsp.buf.definition), opts)
       vim.keymap.set('n', 'gi', fn.flow(center_after_buf_enter, vim.lsp.buf.implementation), opts)
-      vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set('n', 'K', function()
+        vim.cmd'normal zz'
+        vim.lsp.buf.hover()
+      end, opts)
       vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
       vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
       vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
